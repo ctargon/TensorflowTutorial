5b21571ccf773bcabe2bffe6cd44112a0fd39319
Colin Targonski <ctargon@g.clemson.edu>
2018-03-09 13:23:03 -0500

    update

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -34,31 +34,27 @@ def main():
 	# reshape x to be an image input
 	x_image = tf.reshape(x, [-1,28,28,1])
 
-	# regression model computation
-	#y = tf.matmul(x,W) + b
-
+	# initialize weights for first convolutional layer
 	W_conv1 = weight_variable([5, 5, 1, 32])
 	b_conv1 = bias_variable([32])
 
+	# perform convolution operator and max pooling
 	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 	h_pool1 = max_pool_2x2(h_conv1)
 
-	# W_conv2 = weight_variable([5, 5, 32, 64])
-	# b_conv2 = bias_variable([64])
-
-	# h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-	# h_pool2 = max_pool_2x2(h_conv2)
-
-
+	# initialize weights for fully connected layer
 	W_fc1 = weight_variable([14 * 14 * 32, 1024])
 	b_fc1 = bias_variable([1024])
-	#h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+
+	# perform reshaping of the pooling layer and execute fully connected layer
 	h_pool1_flat = tf.reshape(h_pool1, [-1, 14*14*32])
 	h_fc1 = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc1) + b_fc1)
 
+	# initialize weights for last layer
 	W_fc2 = weight_variable([1024, 10])
 	b_fc2 = bias_variable([10])
 
+	# output is y_conv
 	y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
 
 	# define loss function using cross entropy
@@ -73,7 +69,7 @@ def main():
 	sess = tf.InteractiveSession()
 	sess.run(tf.global_variables_initializer())
 
-	# train model for 1000 steps
+	# train model for 500 steps
 	for i in range(500):
 		batch = mnist.train.next_batch(100)
 		train_step.run(feed_dict={x: batch[0], y_: batch[1]})
